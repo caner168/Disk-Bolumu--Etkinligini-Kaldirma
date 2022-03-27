7016d6429a17b5cc6cf0c396342de94d27af89fa
caner168 <83840148+caner168@users.noreply.github.com>
2022-03-28 02:47:20 +0300

    Update Kod3.py

--- Kod3.py
+++ Kod3.py
@@ -1,2 +1,20 @@
 import os
+print("Please Bir Partion Adi Giriniz\n")
+print("-----------------------------\n")
+print("Exampling:D or C or F")
+GetPartion=input("-----------------------\nPartitonName:")
+try:
+    NameDirectory=os.getcwd()
+    CreateUrl=NameDirectory+"\\Kod1.txt"
+    OpeningFile=open(CreateUrl,"r+")
+    ReadingFile=OpeningFile.readlines()
+    CreatingCommand="select volume  "+GetPartion+"\n"
+    ReadingFile.insert(1,CreatingCommand)
+    OpeningFile.seek(0)
+    for loop in ReadingFile:
+        OpeningFile.write(loop)   
+except IOError:
+    print("There is IOError")
+finally:
+    OpeningFile.close()   
 os.system("Kod2.bat")
